df3aed4f8d560d7029d7ed09cca93690c675a15a
Administrateur <janesteve237@gmail.com>
2025-05-06 13:37:04 -0400

    adding an api annotation

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,8 +1,12 @@
 package com.codewithmosh.store;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(servers = {
+        @Server(url = "https://spring-boot-store-app-production.up.railway.app")
+})
 @SpringBootApplication
 public class StoreApplication {
 
