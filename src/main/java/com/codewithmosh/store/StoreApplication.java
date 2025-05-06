f66275047e392e9897903124150aa4d349f61bec
Administrateur <janesteve237@gmail.com>
2025-05-06 09:29:53 -0400

    Bind to dynamic PORT & listen on 0.0.0.0 via Procfile1

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
     public static void main(String[] args) {
+        System.out.println("🛠  PORT env var = '" + System.getenv("PORT") + "'");
         SpringApplication.run(StoreApplication.class, args);
     }
 }
